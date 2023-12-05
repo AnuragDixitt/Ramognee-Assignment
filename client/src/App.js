7349d0732ed078d15cd998bd62325e7942fc4584
AnuragDixitt <anshiva.dixit76@gmail.com>
2023-12-05 15:53:45 +0530

    bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Layout from "./Layout/layout";
+import logo from './logo.svg';
+import './App.css';
+import Layout from './Layout/layout'
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <Layout/>
     </div>
   );
 }
